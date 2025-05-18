a02790ba1ac7697c520faf4d090deaf2320e1631
DianKartiko <auliakartiko28@gmail.com>
2025-05-18 11:38:08 +0700

    Membuat Direktori

---
+++ routes/routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+pages = Blueprint('pages', __name__, template_folder='templates')
+
+@pages.route('/')
+def index():
+    try: 
+        return render_template('index.html')
+    except TemplateNotFound:
+        abort(404)
+
+
